411cbc50f2744860cd3ae5870b78061ea64ed629
vikctar <vikc.steve@gmail.com>
2016-08-04 11:45:17 +0300

    Initial commit

---
+++ app/src/main/java/com/hidic/hidiccs/DiabetesInfo.java
@@ -0,0 +1,13 @@
+package com.hidic.hidiccs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DiabetesInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diabetes_info);
+    }
+}
