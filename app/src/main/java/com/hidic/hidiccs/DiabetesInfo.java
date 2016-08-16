f5a4fc08e5031eb4fbb7e46da1f752cdac2af497
vikctar <vikc.steve@gmail.com>
2016-08-16 23:03:03 +0300

    Add content to HIV and Diabetes actvities

--- app/src/main/java/com/hidic/hidiccs/DiabetesInfo.java
+++ app/src/main/java/com/hidic/hidiccs/DiabetesInfo.java
@@ -2,12 +2,32 @@ package com.hidic.hidiccs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DiabetesInfo extends AppCompatActivity {
+
+    ListView listView;
+    String[] diabetesArray = {"Diabetes Control & Prevention", "Manage/Reduce", "Treatments",
+            "Advice", "Doctor", "Hospital"};
+    List<String> list = new ArrayList<>(Arrays.asList(diabetesArray));
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diabetes_info);
+
+        listView = (ListView) findViewById(R.id.diabetes_list);
+
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
+                R.layout.list_item,
+                R.id.list_item_title,
+                list);
+        listView.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/hidic/hidiccs/HivInfo.java
+++ app/src/main/java/com/hidic/hidiccs/HivInfo.java
@@ -2,12 +2,33 @@ package com.hidic.hidiccs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HivInfo extends AppCompatActivity {
+
+    ListView listView;
+    String[] listItems = {"Prevention", "Manage HIV", "Advice", "Doctor", "Hospital",
+            "Treatment"};
+    ArrayAdapter<String> arrayAdapter;
+    List<String> list = new ArrayList<>(Arrays.asList(listItems));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiv_info);
+
+        listView = (ListView) findViewById(R.id.hiv_list_view);
+
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
+                R.layout.list_item,
+                R.id.list_item_title,
+                list);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
