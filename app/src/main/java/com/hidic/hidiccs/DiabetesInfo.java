31aeefa8d1cf3fac4a6703566e977feea9f9fdee
Roy Walter <Walteniumroy@gmail.com>
2016-08-21 10:49:20 +0300

    maps

--- app/src/main/java/com/hidic/hidiccs/DiabetesInfo.java
+++ app/src/main/java/com/hidic/hidiccs/DiabetesInfo.java
@@ -1,7 +1,9 @@
 package com.hidic.hidiccs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class DiabetesInfo extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class DiabetesInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diabetes_info);
+    }
+
+   public void view(View v) {
+
+        Intent x = new Intent(this, Main3Activity.class);
+        startActivity(x);
     }
 }
--- app/src/main/java/com/hidic/hidiccs/HivInfo.java
+++ app/src/main/java/com/hidic/hidiccs/HivInfo.java
@@ -1,7 +1,9 @@
 package com.hidic.hidiccs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HivInfo extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HivInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiv_info);
+    }
+    public void view(View v) {
+//goes to hivaids map2
+        Intent x = new Intent(this, Main2Activity.class);
+        startActivity(x);
     }
 }
